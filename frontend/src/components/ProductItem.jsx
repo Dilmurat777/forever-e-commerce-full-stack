4c44318ff31cb4d2e9d1a6fbc7609fbbe73b4a13
user.email <mrdilmurat777@gmail.com>
2025-09-27 20:27:55 +0600

    feat: added in page collection items and filter of items

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -8,7 +8,7 @@ const ProductItem = ({ _id, name, image, price }) => {
   return (
 	<Link className="cursor-pointer" to={`/product/${_id}`}>
 		  <div className="overflow-hidden">
-			  <img className="hover:scale-110 transition ease-in-out" src={image[0]} alt="" />
+			  <img className="hover:scale-110 transition ease-in-out" src={image?.[0]} alt="" />
 		  </div>
 		  <p className="pt-3 pb-1 text-sm">{name}</p>
 		  <p className="text-sm font-medium">{currency}{price}</p>
